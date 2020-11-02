6dc49a986274b105ab8d6f11dd75c0db792f05cd
Tailpwnz <tailalekseirozkov@gmail.com>
2020-11-02 15:52:24 +0200

    advantages

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,23 @@
 $(function () {
+   
+ $('.product-slider__inner').slick({
+   arrows:false,
+   dots:true,
+   slidesToShow: 4,
+   slidesToScroll: 4,
+   });
 
-   var mixer = mixitup('.products__inner-box')
+   $('.rate-star').rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+   });
+
+  
+
+   var mixer = mixitup('.products__inner-box');
 
 
-});
+
+})
+
